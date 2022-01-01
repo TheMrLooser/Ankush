486f3ffd04602ee6b0f57648ac9300aa3d74d051
TheMrLooser <88968509+TheMrLooser@users.noreply.github.com>
2022-01-01 16:39:30 +0530

    Update email_collector.py

--- email_collector.py
+++ email_collector.py
@@ -3,14 +3,14 @@ import bs4
 import re
 
 def email_colector():
-    # url = 'https://www.lifewire.com/best-free-email-accounts-1356641'
-    # text  = requests.get(url)
-    # data  = bs4.BeautifulSoup(text.text,'html.parser')
-    # data_string = str(data)
+     url = 'https://www.lifewire.com/best-free-email-accounts-1356641'  # web link from where you want to collect email
+     text  = requests.get(url)
+     data  = bs4.BeautifulSoup(text.text,'html.parser')
+     data_string = str(data)
 
-    strinng = "ankushsoni7171@gmail.com  rahulkuma5000@gmail.com  mzila6482@gmail.com "
+    #strinng = "ankushsoni7171@gmail.com  rahulkuma5000@gmail.com  mzila6482@gmail.com "
 
-    email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+",strinng)
+    email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+",data_string)
     print(email)
     return email
 email_colector()
