7e291773a6c98f7926ec1a6d5f5df8896b08cd19
TheMrLooser <88968509+TheMrLooser@users.noreply.github.com>
2022-01-01 16:46:58 +0530

    Automatic email collector from web

--- email_collector.py
+++ email_collector.py
@@ -8,8 +8,6 @@ def email_colector():
      data  = bs4.BeautifulSoup(text.text,'html.parser')
      data_string = str(data)
 
-    #strinng = "ankushsoni7171@gmail.com  rahulkuma5000@gmail.com  mzila6482@gmail.com "
-
     email = re.findall(r"[0-9a-zA-Z._+%]+@[0-9a-zA-Z._+%]+[.][a-zA-Z.0-9]+",data_string)
     print(email)
     return email
